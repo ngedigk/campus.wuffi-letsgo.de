38927e3b987143a8bb7abcade5e2193cded6b239
Jozze <jozze.w@googlemail.com>
2026-07-17 13:55:26 +0200

    make initial slide navigation sequential

--- src/views/course/sidebar.php
+++ src/views/course/sidebar.php
@@ -2,17 +2,40 @@
     <h2>Modules</h2>
     <ul class="module-list">
         <?php foreach ($modules as $module): ?>
-            <li class="module-item<?= (string)$module['id'] === (string)$currentModule['id'] ? ' active' : '' ?>">
-                <a href="<?= htmlspecialchars($courseService->buildCourseUrl($courseUuid, $module['id'], 0)) ?>">
-                    <?= htmlspecialchars($module['title']) ?>
-                </a>
-                <?php if ($module['id'] === $currentModule['id']): ?>
+            <?php
+                $moduleId = (string)$module['id'];
+                $isCurrentModule = (string)($currentModule['id'] ?? '') === $moduleId;
+                $moduleUnlocked = $isCurrentModule || !empty($moduleUnlockState[$moduleId]);
+            ?>
+            <li class="module-item<?= $isCurrentModule ? ' active' : '' ?><?= $moduleUnlocked ? ' unlocked' : ' locked' ?>">
+                <?php if ($moduleUnlocked || $isCurrentModule): ?>
+                    <a href="<?= htmlspecialchars($courseService->buildCourseUrl($courseUuid, $module['id'], 0)) ?>">
+                        <?= htmlspecialchars($module['title']) ?>
+                    </a>
+                <?php else: ?>
+                    <span class="module-link disabled">
+                        <?= htmlspecialchars($module['title']) ?>
+                    </span>
+                <?php endif; ?>
+                <?php if ($isCurrentModule): ?>
                     <ul class="slide-list">
                         <?php foreach ($slidesForModule as $index => $slide): ?>
-                            <li class="slide-item<?= $index === $currentSlideIndex ? ' active' : '' ?>">
-                                <a href="<?= htmlspecialchars($courseService->buildCourseUrl($courseUuid, $module['id'], $index)) ?>">
-                                    <?= $slide['title'] ?>
-                                </a>
+                            <?php
+                                $slideId = (string)($slide['id'] ?? '');
+                                $isCurrentSlide = !empty($currentSlide) && (string)$currentSlide['id'] === $slideId;
+                                $isUnlocked = $slideId === '' || !empty($slideUnlockState[$slideId]);
+                                $isVisited = $slideId !== '' && !empty($viewedSlideIds[$slideId]);
+                            ?>
+                            <li class="slide-item<?= $isCurrentSlide ? ' active' : '' ?><?= $isVisited ? ' visited' : '' ?><?= $isUnlocked ? ' unlocked' : ' locked' ?>">
+                                <?php if ($isCurrentSlide || $isUnlocked): ?>
+                                    <a href="<?= htmlspecialchars($courseService->buildCourseUrl($courseUuid, $module['id'], $index)) ?>">
+                                        <?= htmlspecialchars($slide['title']) ?>
+                                    </a>
+                                <?php else: ?>
+                                    <span class="slide-link disabled">
+                                        <?= htmlspecialchars($slide['title']) ?>
+                                    </span>
+                                <?php endif; ?>
                             </li>
                         <?php endforeach; ?>
                     </ul>
