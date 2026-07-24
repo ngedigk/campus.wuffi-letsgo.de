8d925004fce2e0b08985d27d5faef4057255359a
Jozze <jozze.w@gmail.com>
2026-07-24 08:26:06 +0200

    remove rate_limit require

--- src/index.php
+++ src/index.php
@@ -2,7 +2,6 @@
 require_once __DIR__ . '/autoload.php';
 require_once __DIR__ . '/auth.php';
 require_once __DIR__ . '/csrf.php';
-require_once __DIR__ . '/rate_limit.php';
 require_once __DIR__ . '/Container.php';
 
 $container = Container::getInstance();
